a36641e9ae0567fc92cebb94000476d2311d2116
tehbeard <tehbeard@gmail.com>
2026-01-19 09:40:59 +0000

    refactor: Add reusable panel with header.

---
+++ src/main/java/com/tehbeard/fabric/quickaction/ui/component/PanelWithHeader.java
@@ -0,0 +1,40 @@
+package com.tehbeard.fabric.quickaction.ui.component;
+
+import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
+import io.github.cottonmc.cotton.gui.widget.WLabel;
+import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
+import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
+import io.github.cottonmc.cotton.gui.widget.data.Insets;
+import io.github.cottonmc.cotton.gui.widget.data.Texture;
+import io.github.cottonmc.cotton.gui.widget.data.VerticalAlignment;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class PanelWithHeader extends WPixelPanel {
+
+    public PanelWithHeader(String heading, int width, int height) {
+        this.setInsets(Insets.NONE);
+
+        this.setBackgroundPainter(BackgroundPainter.createNinePatch(
+            new Texture(Identifier.of("quickmenu", "textures/background.png")),
+            builder -> builder.cornerSize(8).cornerUv(0.33f)
+        ));
+
+        WPlainPanel header = new WPlainPanel();
+        header.setInsets(Insets.NONE);
+        header.setBackgroundPainter(BackgroundPainter.createNinePatch(
+            new Texture(Identifier.of("quickmenu", "textures/background_header.png")),
+            builder -> builder.cornerSize(8).cornerUv(0.33f)
+        ));
+        header.setSize(this.getWidth(),24);
+        this.add(header, 0,0);
+
+        WLabel label = new WLabel(Text.literal(heading), 0xFF_FFFFFF);
+        label.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        label.setVerticalAlignment(VerticalAlignment.CENTER);
+
+        header.add(label, (this.getWidth() / 2) - 9,3);
+    }
+
+
+}
